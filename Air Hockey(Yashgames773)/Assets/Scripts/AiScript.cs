2b9a3f5a36c4ec36e21f8dadbc4a91e99a8b666f
Yash Gupta <yashgames773@gmail.com>
2023-05-02 18:03:20 +0530

    ai implementation

---
+++ Air Hockey(Yashgames773)/Assets/Scripts/AiScript.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using static Player_Movement;
+
+public class AiScript : MonoBehaviour
+{
+    public float maxMoveSpeed;
+    private Rigidbody2D rb2D;
+    private Vector2 startPos;
+
+    public Transform player2BboundaryHolder;
+    private Player_Movement.Boundary player2Boundary;
+
+    public Rigidbody2D puckRb2d;
+    public Transform puckBboundaryHolder;
+    private Vector2 targetPos;
+    private Player_Movement.Boundary puckBoundary;
+
+    private bool isFirstTimeInOpponentsHalf = true;
+    private float offsetXFromTarget;
+
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        startPos = rb2D.position;
+
+        player2Boundary = new Boundary(player2BboundaryHolder.GetChild(0).position.y, player2BboundaryHolder.GetChild(1).position.y, //take the all the Boundaries position in their coordinates
+                                    player2BboundaryHolder.GetChild(2).position.x, player2BboundaryHolder.GetChild(3).position.x);
+
+
+        puckBoundary = new Boundary(puckBboundaryHolder.GetChild(0).position.y, puckBboundaryHolder.GetChild(1).position.y, //take the all the Boundaries position in their coordinates
+                                   puckBboundaryHolder.GetChild(2).position.x, puckBboundaryHolder.GetChild(3).position.x);
+    }
+
+    private void FixedUpdate()
+    {
+        float puckMoveSpeed;
+
+
+        if(puckRb2d.position.y < puckBoundary.Down)
+        {
+
+            if (isFirstTimeInOpponentsHalf)
+            {
+                isFirstTimeInOpponentsHalf = false;
+                offsetXFromTarget = Random.Range(-1f, 1f);
+            }
+            puckMoveSpeed = maxMoveSpeed * Random.Range(0.1f, 0.4f);
+
+
+            targetPos = new Vector2(Mathf.Clamp(puckRb2d.position.x + offsetXFromTarget, puckBoundary.Left,puckBoundary.Right),
+                                                startPos.y);    
+        }
+
+        else
+        {
+            puckMoveSpeed = Random.Range(maxMoveSpeed * 0.5f, maxMoveSpeed);
+            targetPos = new Vector2(Mathf.Clamp(puckRb2d.position.x, puckBoundary.Left, puckBoundary.Right),
+                                    Mathf.Clamp(puckRb2d.position.y, puckBoundary.Down, puckBoundary.Up));
+
+
+        }
+            rb2D.MovePosition(Vector2.MoveTowards(rb2D.position,targetPos,puckMoveSpeed * Time.fixedDeltaTime));
+    }
+
+
+}
+
--- Air Hockey(Yashgames773)/Assets/Scripts/Player_Movement.cs
+++ Air Hockey(Yashgames773)/Assets/Scripts/Player_Movement.cs
@@ -6,17 +6,17 @@ public class Player_Movement : MonoBehaviour
 {
     
     bool wasJustClicked = true; //Player Clicked the mouseButton
-    bool canMove; //
-    Vector2 playerSize; //for Size of the sprite 
+    bool canMove;
+    Collider2D playerCollider;
     public Transform boundaryHolder; //It will get the Boundary Position of all the direction
     Rigidbody2D rb2d;
 
     Boundary playerBoundary;
 
-    struct Boundary //Remembering the Position of boundaries
+   public struct Boundary //Remembering the Position of boundaries
     {
         public float Up, Down, Left, Right;
-
+       
         public Boundary(float up, float down, float left, float right)
         {
             Up = up; Down = down; Left = left; Right = right; //Declaring the min and max pos in mathf.clamp for restricting the position
@@ -27,9 +27,9 @@ public class Player_Movement : MonoBehaviour
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        playerSize = GetComponentInChildren<SpriteRenderer>().bounds.extents; 
+        playerCollider = GetComponent<Collider2D>();
 
-        playerBoundary = new Boundary(boundaryHolder.GetChild(0).position.y, boundaryHolder.GetChild(1).position.y, //take the all the Boundaries position in their coordinates
+         playerBoundary = new Boundary(boundaryHolder.GetChild(0).position.y, boundaryHolder.GetChild(1).position.y, //take the all the Boundaries position in their coordinates
                                       boundaryHolder.GetChild(2).position.x, boundaryHolder.GetChild(3).position.x);
     }
 
@@ -44,8 +44,7 @@ public class Player_Movement : MonoBehaviour
             {
                 wasJustClicked = false; 
 
-                if (mousePos.x > transform.position.x - playerSize.x && mousePos.x < transform.position.x + playerSize.x && //Only it will  move if the player is inside the puck 
-               mousePos.y > transform.position.y - playerSize.y && mousePos.y < transform.position.y + playerSize.y)
+                if (playerCollider.OverlapPoint(mousePos))
                 {
                     canMove = true;
                 }
@@ -54,6 +53,7 @@ public class Player_Movement : MonoBehaviour
                     canMove = false;
                 }
             }
+         
 
             if (canMove)
             {
